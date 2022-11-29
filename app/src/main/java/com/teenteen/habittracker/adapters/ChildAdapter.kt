2d2b20a311d06aefa13889def5063294d6a136ef
emikoo <emikoo@users.noreply.github.com>
2022-11-29 02:28:46 +0100

    added select check animation

--- app/src/main/java/com/teenteen/habittracker/adapters/ChildAdapter.kt
+++ app/src/main/java/com/teenteen/habittracker/adapters/ChildAdapter.kt
@@ -15,11 +15,14 @@ import com.teenteen.habittracker.data.model.Habit
 
 open class ChildAdapter(var dataSet: List<Habit.Day>, val listener: OnItemSelected): RecyclerView.Adapter<ChildAdapter.ChildViewHolder>() {
 
+    lateinit var avd: AnimatedVectorDrawableCompat
+    lateinit var avd2: AnimatedVectorDrawable
 
     inner class ChildViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val name: TextView = itemView.findViewById(R.id.title_child)
         val number: TextView = itemView.findViewById(R.id.subtitle_child)
         val card: CardView = itemView.findViewById(R.id.card)
+        val image: ImageView = itemView.findViewById(R.id.iv_done)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup , viewType: Int) = ChildViewHolder(
@@ -47,6 +50,18 @@ open class ChildAdapter(var dataSet: List<Habit.Day>, val listener: OnItemSelect
             holder.name.setTextColor(holder.itemView.resources.getColor(R.color.white))
             holder.number.visibility = View.GONE
             holder.image.visibility = View.VISIBLE
+            val drawable = holder.image.drawable
+            if (drawable is AnimatedVectorDrawableCompat && item.isSelected == 1) {
+                item.isSelected = 2
+                avd = drawable as AnimatedVectorDrawableCompat
+                avd.start()
+            } else if (drawable is AnimatedVectorDrawable && item.isSelected == 1) {
+                item.isSelected = 2
+                avd2 = drawable as AnimatedVectorDrawable
+                avd2.start()
+            } else if (item.isSelected == 2) {
+                holder.image.setImageResource(R.drawable.ic_done)
+            }
             holder.card.setCardBackgroundColor(holder.itemView.resources.getColor(R.color.orange))
         }
     }
